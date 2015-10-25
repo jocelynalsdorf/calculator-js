52a7e632e0ab41a5a61dde365d8b3db10364e3da
jocelynalsdorf <jfrodsla@gmail.com>
2015-10-25 13:07:00 -0700

    fix bug where valOne was always 0; addition working, subtraction not

--- app.js
+++ app.js
@@ -15,36 +15,46 @@
     $("#display").data("fromPrevious", false);
   }
     
-
+    // resetCalc('0');
     $(".on-button").click(function(){ });//close on-btn click function
     $(".off-button").click(function(){});
 
-
+(function(){
+    resetCalc('0');
 
     $(".num-button").click(function(){
+   
     //has a calculation just been performed: t or f?
       if($("#display").data("fromPrevious") == true) {
         resetCalc($(this).text());
+        console.log("first if");
     //if calc has not just been performed and first num hasnt been locked (no fx btn click)
       } else if(($("#display").data("isPendingFunction") == true) && ($("#display").data("valueOneLocked") == false)) {
+         console.log("first else if");
         //sets valueOne to what is currently in display and stores in .data()
           $("#display").data("valueOne", $("#display").val()); 
         //change boolean in .data() from f to t
           $("#display").data("valueOneLocked", true); 
+          console.log($("#display").data("valueOne"));
         //set the display value to the text of the clicked num-btn
-          $("#display").data($(this).text()); 
+          $("#display").val($(this).text()); 
         //set valueTwo val in .data() to the new display value
           $("#display").data("valueTwo", $("#display").val()); 
+          console.log($("#display").data("valueTwo"));
         // change boolean in .data from f to t
           $("#display").data("valueTwoLocked", true); 
+
+
     //click a number again after first num locked and a val exists for second num
       } else if(($("#display").data("isPendingFunction")) == true && ($("#display").data("valueOneLocked") == true)) {
+             console.log("second else if");
         //need to add the two vals together and reassign numbertwo val
         var curValue = $("#display").val();
         //the last number btn clicked
         var toAdd = $(this).text(); 
         //concat the 2 strings
         var newValue = curValue + toAdd; 
+        $("#display").val(newValue);
         //assign the newValue to valueTwo in .data() so a fx can be done on it
         $("#display").data("valueTwo", $("#display").val());
         //lock the 2nd num changing from f to t
@@ -52,11 +62,15 @@
     //clicking on a number without any prior behaviors
       } else {
         //set curVal to whatever is in display
+        //console.log($("#display").val());
         var curValue = $("#display").val();
+       // console.log(curValue);
         //if no num by user yet, prevent 0 from staying in display when  new num clicked
         if(curValue == "0") {
           curValue = "";
+
         }
+
         //allow multiple nums to be shown on display
         var toAdd = $(this).text();
         //concat the vals so displayed properly 
@@ -69,7 +83,9 @@
 
    
 
-    $(".function-button").click(function(){
+    $(".function-button").click(function(e){
+       e.preventDefault();
+      console.log($("#display").data("valueOne"));
     //when fx-btn clicked, lock first num so that num btns know to start second ('fromPrevious:t' means a calc just happened)
       if($("#display").data("fromPrevious") == true) {
         //if calc just happened rest calc using the current display as current input val
@@ -92,7 +108,8 @@
       $(this).addClass("pendingFunction");  
     });
 
-    $(".equals-button").click(function(){
+    $(".equals-button").click(function(e){
+      e.preventDefault();
     //first check if we are ready to do a calc (if both nums are locked)
       if(($("#display").data("valueOneLocked")) == true &&  ($("#display").data("valueTwoLocked") == true)) {
         if($("#display").data("thePendingFunction") == "+") {
@@ -113,7 +130,7 @@
         } else if($("#display").data("thePendingFunction") == "&radic;") {
           //get sq root
           //$("#display").data("fromPrevious", true)
-          var finalValue = Math.sqrt(parseFloat($("#display").data("curValue"));
+          var finalValue = Math.sqrt(parseFloat($("#display").data("curValue")));
         }
 
         //show results
@@ -127,12 +144,14 @@
       }
 
     });
-    $(".clear-button").click(function(){});
+    $(".clear-button").click(function(){
+       resetCalc("0"); 
+    });
 
 
 
 
-
+})();
 
 //drag functionality
     $("#calculator").draggable(); 
