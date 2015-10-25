6910725bdeba4a248be28424ec0b652fd2d8c8f2
jocelynalsdorf <jfrodsla@gmail.com>
2015-10-25 08:55:33 -0700

    modify to have on off buttons; set up basic jquery click fx

---
+++ app.js
@@ -0,0 +1,13 @@
+  $(document).ready(function(){
+
+    $(".num-button").click(function(){});
+
+    $(".clear-button").click(function(){});
+
+    $(".function-button").click(function(){});
+
+    $(".equals-button").click(function(){});
+
+
+
+  });//end of doc
